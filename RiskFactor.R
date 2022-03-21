8f72f5912304665a730dd2c1bf607257bf7818c4
Hong <chong@clevelandohio.gov>
2022-03-21 15:12:27 -0400

    update

--- RiskFactor.R
+++ RiskFactor.R
@@ -4,24 +4,21 @@ options(knitr.table.format = "latex")
 rm(list=ls())
 
 library(tidyverse)
-
 #install.packages("BAMMtools")
-
 library(BAMMtools)
+library(dplyr)
 library(ggplot2)
 
-# Work directory Setup            #
-#getwd()
-#dd = c("c:/Rwork/")
-#setwd(dd)
 getwd()
 
-mydata <- read.csv("PR02ListActivitiesbyProgramYearProject.csv", header = TRUE)
+mydata <- read.csv("PR02ListActivitiesbyProgramYearandProject03182022.csv", header = TRUE)
 
-mydata1 <- subset(mydata, IDIS.Activity.ID !=15079)
+#why ? CDPH Admin
+mydata1 <- subset(mydata, IDIS.Activity.ID !=15380)
 
+# why? 12 Land retutilization, 13 Fair Housing Admin, 14 General Program Admin, 15 Section 108 Loans
 mydata2 <- mydata1 %>%
-  filter(Funded.Amount > 0 & IDIS.Project < 12) 
+  filter(Funded.Amount > 0 & IDIS.Project < 12)  
 
 
 fund.mean <- mean(mydata2$Funded.Amount)
@@ -125,9 +122,9 @@ mydata2$RiskFactor.cat.desc [mydata2$RiskFactor.cat ==3] <- "1. High Risk"
 
 table (mydata2$RiskFactor.cat)
 
-# Random Sampling
-#high risk - 5
-#Medium risk - 4
+# Random Sampling for field monitoring
+#high risk - 2
+#Medium risk - 1
 #Low risk - 1
 #select & mark
 
@@ -140,75 +137,165 @@ mydata2.medium <- mydata2 %>%
 mydata2.high <- mydata2 %>%
   filter(RiskFactor.cat ==3) 
 
+
 mysample.low <- sample(mydata2.low$IDIS.Activity.ID, size = 1)
 mysample.low
 
-mysample.med <- sample(mydata2.medium$IDIS.Activity.ID, size = 4)
+mysample.med <- sample(mydata2.medium$IDIS.Activity.ID, size = 1)
 mysample.med
 
-mysample.high <- sample(mydata2.high$IDIS.Activity.ID, size = 5)
+mysample.high <- sample(mydata2.high$IDIS.Activity.ID, size = 2)
 mysample.high
+
+
+# make a group for selected
+
+monitoringlist <- c(mysample.high, mysample.med, mysample.low)
+
+monitoringlist
+length(monitoringlist)
 
 
 #update selection on data
 mydata2$Sel <- 0
 
-# low risk selection update
-for (i in 1: length(mydata2$Sel))
-{
-  if (mydata2$IDIS.Activity.ID [i] == mysample.low [1]) {
-    mydata2$Sel [i] <- 1
-  } 
-}
 
-#medium risk selection update
-for (i in 1: length(mydata2$Sel))
-{
-  if (mydata2$IDIS.Activity.ID [i] == mysample.med [1]) {
-    mydata2$Sel [i] <- 2
-  } else if (mydata2$IDIS.Activity.ID [i] == mysample.med [2]) {
-    mydata2$Sel [i] <- 2
-  } else if (mydata2$IDIS.Activity.ID [i] == mysample.med [3]) {
-    mydata2$Sel [i] <- 2
-  } else if (mydata2$IDIS.Activity.ID [i] == mysample.med [4]) {
-    mydata2$Sel [i] <- 2
-  } 
-}
+# risk selection update
 
-#high risk selection update
-for (i in 1: length(mydata2$Sel))
-{
-  if (mydata2$IDIS.Activity.ID [i] == mysample.high [1]) {
-    mydata2$Sel [i] <- 3
-  } else if (mydata2$IDIS.Activity.ID [i] == mysample.high [2]) {
-    mydata2$Sel [i] <- 3
-  } else if (mydata2$IDIS.Activity.ID [i] == mysample.high [3]) {
-    mydata2$Sel [i] <- 3
-  } else if (mydata2$IDIS.Activity.ID [i] == mysample.high [4]) {
-    mydata2$Sel [i] <- 3
-  } else if (mydata2$IDIS.Activity.ID [i] == mysample.high [5]) {
-    mydata2$Sel [i] <- 3
-  } 
+for (i in 1: length(monitoringlist)) {
+
+  for (j in 1: length(mydata2$Sel))
+  {
+    if (mydata2$IDIS.Activity.ID [j] == monitoringlist[i]) {
+        mydata2$Sel [j] <- 1
+      } 
+  }
+
 }
 
 table (mydata2$Sel)
 
-write.csv(mydata2, file="PR02RiskFactor.csv", row.names = FALSE, na="")
+
+mydata2.selected <- mydata2 %>%filter(Sel ==1) 
+
+
+write.csv(mydata2.selected, file="MonitoringList.csv", row.names = FALSE, na="")
+
+
+
+#-------------------------------------------------------------------
+
+mydata3 <- mydata2 %>%filter(Sel ==0)
+
+
+mydata3.low <- mydata3 %>%
+  filter(RiskFactor.cat ==1) 
+
+mydata3.medium <- mydata3 %>%
+  filter(RiskFactor.cat ==2) 
+
+mydata3.high <- mydata3 %>%
+  filter(RiskFactor.cat ==3) 
+
+
+mysample.low.desk <- sample(mydata3.low$IDIS.Activity.ID, size = 1)
+mysample.low.desk
+
+mysample.med.desk <- sample(mydata3.medium$IDIS.Activity.ID, size = 2)
+mysample.med.desk
+
+mysample.high.desk <- sample(mydata3.high$IDIS.Activity.ID, size = 4)
+mysample.high.desk
+
+
+# make a group for selected
+
+deskmonitoringlist <- c(mysample.high.desk, mysample.med.desk, mysample.low.desk)
+
+deskmonitoringlist
+length(deskmonitoringlist)
+
+
+#update selection on data
+mydata3$deskSel <- 0
+
+
+# risk selection update
+
+for (i in 1: length(deskmonitoringlist)) {
+  
+  for (j in 1: length(mydata3$deskSel))
+  {
+    if (mydata3$IDIS.Activity.ID [j] == deskmonitoringlist[i]) {
+      mydata3$deskSel [j] <- 1
+    } 
+  }
+  
+}
+
+table (mydata3$deskSel)
+
+
+mydata3.selected <- mydata3 %>%filter(deskSel ==1) 
+
+
+write.csv(mydata3.selected, file="DesktopMonitoringList.csv", row.names = FALSE, na="")
 
 
 # plot
-theme_set(theme_bw())
-data(mydata2, package = "ggplot2")
 myplot <- ggplot (mydata2, aes(x=Award.Ratio, y=DrawRate))+
-                geom_point(aes(col=RiskFactor.cat.desc, size=RiskFactor)) +
-                labs(subtitle = "Award Risk vs Performance Risk", 
-                 y= "Performance Risk",
-                 x= "Award Risk",
-               title = "Risk Factor Analysis",
-               caption = "Source: HUD PR02 Report as of Oct 25, 2021")
+  geom_point(aes(col=RiskFactor.cat.desc, size=RiskFactor)) +
+  geom_text(data=mydata2.selected, aes(label= IDIS.Activity.ID), color="black")+
+  #geom_text(data=mydata3.selected, aes(label= IDIS.Activity.ID), color="black")+
+  labs(subtitle = "Field Monitoring List 4 in 1 Q 2022", 
+       y= "Performance Risk",
+       x= "Award Risk",
+       title = "Risk Factor Analysis",
+       caption = "Source: HUD PR02 Report as of March 18, 2022")+
+  theme_minimal()
 
-         
 plot(myplot)
 
 
+myplot2 <- ggplot (mydata2, aes(x=Award.Ratio, y=DrawRate))+
+  geom_point(aes(col=RiskFactor.cat.desc, size=RiskFactor)) +
+  #geom_text(data=mydata2.selected, aes(label= IDIS.Activity.ID), color="black")+
+  geom_text(data=mydata3.selected, aes(label= IDIS.Activity.ID), color="black")+
+  labs(subtitle = "Desktop Monitoring List 7 in 1 Q 2022", 
+       y= "Performance Risk",
+       x= "Award Risk",
+       title = "Risk Factor Analysis",
+       caption = "Source: HUD PR02 Report as of March 18, 2022")+
+  theme_minimal()
 
+plot(myplot2)
+
+
+myplot3 <- ggplot (mydata2.selected, aes(x=Award.Ratio, y=DrawRate))+
+  geom_point(aes(col=RiskFactor.cat.desc, size=RiskFactor)) +
+  geom_text(aes(label= IDIS.Activity.ID), color="black")+
+  labs(title = "Risk Factor Analysis", 
+       subtitle = "Selected Field Monitor", 
+       y= "Performance Risk",
+       x= "Award Risk",
+       caption = "Source: HUD PR02 Report as of March 18, 2022")+
+       theme_minimal()
+
+
+
+plot(myplot3)
+
+
+myplot4 <- ggplot (mydata3.selected, aes(x=Award.Ratio, y=DrawRate))+
+  geom_point(aes(col=RiskFactor.cat.desc, size=RiskFactor)) +
+  geom_text(aes(label= IDIS.Activity.ID), color="black")+
+  labs(title = "Risk Factor Analysis", 
+       subtitle = "Selected Desktop Monitor", 
+       y= "Performance Risk",
+       x= "Award Risk",
+       caption = "Source: HUD PR02 Report as of March 18, 2022")+
+      theme_minimal()
+
+
+
+plot(myplot4)
